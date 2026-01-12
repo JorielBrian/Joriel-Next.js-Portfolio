e90c20fb8dfc01d4dc57d2c8cb4af7855c86b096
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-01-12 23:58:43 +0800

    fixed

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -7,7 +7,7 @@ const LoadingPage: React.FC = () => {
       <h1 className="text-2xl xl:text-8xl!!! font-semibold text-gray-400 mb-4">Loading Portfolio</h1>
       <div className="w-64 h-2 rounded-full overflow-hidden">
         <div 
-          className="h-full w-1/2 bg-gradient-to-r from-blue-400 to-blue-800"
+          className="h-full w-1/2 bg-linear-to-r from-blue-400 to-blue-800"
           style={{
             animation: 'slide 1.5s ease-in-out infinite',
           }}
