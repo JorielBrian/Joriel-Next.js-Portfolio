15bafb728be534ed5934c9cade73029f1a44ed65
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-01-12 22:49:49 +0800

    added a project, and organize images

--- app/data/all_skills.ts
+++ app/data/all_skills.ts
@@ -4,77 +4,77 @@ import { Proficiency, Preference, Category } from "./enum";
 export const SKILLS: Skill[] = [
     // Core Skills
     
-    {skill:"Next.js", focus: true, image: "/next.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
-    {skill:"Laravel", focus: true, image: "/laravel.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
-    {skill:"React", focus: true, image: "/react.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
-    {skill:"TypeScript", focus:true, image: "/typescript.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.LANGUAGE},
-    {skill:"Tailwind CSS", focus:true, image: "/tailwind.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
-    {skill:"GIT", focus:true, image: "/git.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.VERSION_CONTROL},
-    {skill:"Vercel", focus:true, image: "/vercel.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.VERSION_CONTROL},
+    {skill:"Next.js", focus: true, image: "/Skills/next.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
+    {skill:"Laravel", focus: true, image: "/Skills/laravel.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
+    {skill:"React", focus: true, image: "/Skills/react.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
+    {skill:"TypeScript", focus:true, image: "/Skills/typescript.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.LANGUAGE},
+    {skill:"Tailwind CSS", focus:true, image: "/Skills/tailwind.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.FRAMEWORK},
+    {skill:"GIT", focus:true, image: "/Skills/git.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.VERSION_CONTROL},
+    {skill:"Vercel", focus:true, image: "/Skills/vercel.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.VERSION_CONTROL},
 
     // Tech Stack
-    {skill:"T3 Stack", image: "/t3.svg", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.STACK},
-    {skill:"TALL Stack", image: "/tall.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.STACK},
-    {skill:"Livewire", image: "/livewire.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.FRAMEWORK},
-    {skill:"Eloquent", image: "/eloquent.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.ORM},
+    {skill:"T3 Stack", image: "/Skills/t3.svg", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.STACK},
+    {skill:"TALL Stack", image: "/Skills/tall.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.STACK},
+    {skill:"Livewire", image: "/Skills/livewire.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.FRAMEWORK},
+    {skill:"Eloquent", image: "/Skills/eloquent.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.ORM},
 
     // Fundamental Skills
-    {skill:"PHP", image: "/php.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.FUNDAMENTAL },
-    {skill:"HTML", image: "/html.png", proficiency:Proficiency.ADVANCE, preference:Preference.OTHER, category:Category.FUNDAMENTAL},
-    {skill:"CSS", image: "/css.png", proficiency:Proficiency.ADVANCE, preference:Preference.OTHER, category:Category.FUNDAMENTAL},
+    {skill:"PHP", image: "/Skills/php.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.FUNDAMENTAL },
+    {skill:"HTML", image: "/Skills/html.png", proficiency:Proficiency.ADVANCE, preference:Preference.OTHER, category:Category.FUNDAMENTAL},
+    {skill:"CSS", image: "/Skills/css.png", proficiency:Proficiency.ADVANCE, preference:Preference.OTHER, category:Category.FUNDAMENTAL},
 
     // Languages
-    {skill:"JavaScript", image: "/javascript.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.LANGUAGE},
-    {skill:"Python", image: "/python.png", proficiency:Proficiency.BEGINNER, preference:Preference.OTHER, category:Category.LANGUAGE},
-    {skill:"R", image: "/r.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.LANGUAGE},
+    {skill:"JavaScript", image: "/Skills/javascript.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.LANGUAGE},
+    {skill:"Python", image: "/Skills/python.png", proficiency:Proficiency.BEGINNER, preference:Preference.OTHER, category:Category.LANGUAGE},
+    {skill:"R", image: "/Skills/r.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.LANGUAGE},
 
     // Frameworks and CLIs
-    {skill:"Bootstrap", image: "/bootstrap.png", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.FRAMEWORK},
-    {skill:"WordPress", image: "/wordpress.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.CLI},
-    {skill:"Express.js", image: "/expressjs.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.FRAMEWORK},
-    {skill:"Ubuntu Linux", image: "/ubuntu.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.OS},
+    {skill:"Bootstrap", image: "/Skills/bootstrap.png", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.FRAMEWORK},
+    {skill:"WordPress", image: "/Skills/wordpress.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.CLI},
+    {skill:"Express.js", image: "/Skills/expressjs.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.FRAMEWORK},
+    {skill:"Ubuntu Linux", image: "/Skills/ubuntu.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.OS},
 
     // Backend 
-    {skill:"SQL", image: "/sql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.BACKEND},
-    {skill:"Node.js", image: "/nodejs.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.BACKEND},
-    {skill:"tRPC", image: "/trpc.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.BACKEND},
+    {skill:"SQL", image: "/Skills/sql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.CORE_SKILL, category:Category.BACKEND},
+    {skill:"Node.js", image: "/Skills/nodejs.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.BACKEND},
+    {skill:"tRPC", image: "/Skills/trpc.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.BACKEND},
 
     // Databases
-    {skill:"PostgreSQL", image: "/postgresql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.DATABASE},
-    {skill:"MySQL", image: "/mysql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.DATABASE},
-    {skill:"SQLite", image: "/sqlite.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.DATABASE},
-    {skill:"Firebase", image: "/firebase.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.DATABASE},
+    {skill:"PostgreSQL", image: "/Skills/postgresql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.DATABASE},
+    {skill:"MySQL", image: "/Skills/mysql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.DATABASE},
+    {skill:"SQLite", image: "/Skills/sqlite.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.DATABASE},
+    {skill:"Firebase", image: "/Skills/firebase.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.DATABASE},
 
     // ORMs
-    {skill:"Prisma", image: "/prisma.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.ORM},
-    {skill:"GraphQL", image: "/graphql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.ORM},
-
+    {skill:"Prisma", image: "/Skills/prisma.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.ORM},
+    {skill:"GraphQL", image: "/Skills/graphql.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.ORM},
     // Tools
-    {skill:"GitHub", image: "/github.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.VERSION_CONTROL},
-    {skill:"Vite", image: "/vite.svg", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.OTHER},
-    {skill:"Hostinger", image: "/hostinger.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.VERSION_CONTROL},
-    {skill:"Flux UI", image: "/flux.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.TOOLS},
-    {skill:"FileZilla", image: "/filezilla.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.VERSION_CONTROL},
-    {skill:"Shiny", image: "/shiny.png", proficiency:Proficiency.BEGINNER, preference:Preference.OTHER, category:Category.TOOLS},
-    {skill:"Sketchup", image: "/sketchup.png", proficiency:Proficiency.BEGINNER, preference:Preference.OTHER, category:Category.GRAPHICS},
-    {skill:"Cube Dynasm", image: "/cube_dynasm.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.TOOLS},
+    {skill:"GitHub", image: "/Skills/github.png", proficiency:Proficiency.PROFICIENT, preference:Preference.CORE_SKILL, category:Category.VERSION_CONTROL},
+    {skill:"Vite", image: "/Skills/vite.svg", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.OTHER},
+    {skill:"Hostinger", image: "/Skills/hostinger.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.SUB_SKILL, category:Category.VERSION_CONTROL},
+    {skill:"Flux UI", image: "/Skills/flux.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.OTHER, category:Category.TOOLS},
+    {skill:"FileZilla", image: "/Skills/filezilla.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.VERSION_CONTROL},
+    {skill:"Shiny", image: "/Skills/shiny.png", proficiency:Proficiency.BEGINNER, preference:Preference.OTHER, category:Category.TOOLS},
+    {skill:"Sketchup", image: "/Skills/sketchup.png", proficiency:Proficiency.BEGINNER, preference:Preference.OTHER, category:Category.GRAPHICS},
+    {skill:"Cube Dynasm", image: "/Skills/cube_dynasm.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.OTHER, category:Category.TOOLS},
     
     // Hardware Skills
-    {skill:"Computer Assembling", image: "/computerassembling.png", proficiency:Proficiency.PROFICIENT, preference:Preference.HARDWARE, category:Category.HARDWARE},
-    {skill:"Circuit Assembling", image: "/circuitassembling.png", proficiency:Proficiency.PROFICIENT, preference:Preference.HARDWARE, category:Category.HARDWARE},
-    {skill:"Troubleshooting", image: "/troubleshooting.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.HARDWARE, category:Category.HARDWARE},
-    {skill:"Soldering", image: "/soldering.png", proficiency:Proficiency.PROFICIENT, preference:Preference.HARDWARE, category:Category.HARDWARE},
+    {skill:"Computer Assembling", image: "/Skills/computerassembling.png", proficiency:Proficiency.PROFICIENT, preference:Preference.HARDWARE, category:Category.HARDWARE},
+    {skill:"Circuit Assembling", image: "/Skills/circuitassembling.png", proficiency:Proficiency.PROFICIENT, preference:Preference.HARDWARE, category:Category.HARDWARE},
+    {skill:"Troubleshooting", image: "/Skills/troubleshooting.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.HARDWARE, category:Category.HARDWARE},
+    {skill:"Soldering", image: "/Skills/soldering.png", proficiency:Proficiency.PROFICIENT, preference:Preference.HARDWARE, category:Category.HARDWARE},
 
     // Microsoft Skills
-    {skill:"Word", image: "/word.png", proficiency:Proficiency.PROFICIENT, preference:Preference.SOFT_SKILLS, category:Category.TOOLS},
-    {skill:"Powerpoint", image: "/powerpoint.png", proficiency:Proficiency.PROFICIENT, preference:Preference.SOFT_SKILLS, category:Category.TOOLS},
-    {skill:"Excel", image: "/excel.png", proficiency:Proficiency.PROFICIENT, preference:Preference.SOFT_SKILLS, category:Category.TOOLS},
+    {skill:"Word", image: "/Skills/word.png", proficiency:Proficiency.PROFICIENT, preference:Preference.SOFT_SKILLS, category:Category.TOOLS},
+    {skill:"Powerpoint", image: "/Skills/powerpoint.png", proficiency:Proficiency.PROFICIENT, preference:Preference.SOFT_SKILLS, category:Category.TOOLS},
+    {skill:"Excel", image: "/Skills/excel.png", proficiency:Proficiency.PROFICIENT, preference:Preference.SOFT_SKILLS, category:Category.TOOLS},
 
     // Design Skills
-    {skill:"Canva", image: "/canva.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.DESIGN, category:Category.GRAPHICS},
-    {skill:"Capcut", image: "/capcut.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.DESIGN, category:Category.GRAPHICS},
-    {skill:"Powerdirector", image: "/powerdirector.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.DESIGN, category:Category.GRAPHICS},
+    {skill:"Canva", image: "/Skills/canva.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.DESIGN, category:Category.GRAPHICS},
+    {skill:"Capcut", image: "/Skills/capcut.png", proficiency:Proficiency.INTERMEDIATE, preference:Preference.DESIGN, category:Category.GRAPHICS},
+    {skill:"Powerdirector", image: "/Skills/powerdirector.png", proficiency:Proficiency.FUNCTIONAL, preference:Preference.DESIGN, category:Category.GRAPHICS},
 
     // Other Skills
-    {skill:"Facebook", image: "/facebook.png", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.SOCIAL_MEDIA},
+    {skill:"Facebook", image: "/Skills/facebook.png", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.SOCIAL_MEDIA},
+    {skill:"Software", image: "/Skills/software.png", proficiency:Proficiency.PROFICIENT, preference:Preference.OTHER, category:Category.OTHER}
 ];
--- app/data/projects.ts
+++ app/data/projects.ts
@@ -3,7 +3,7 @@ import { Project } from "./types"
 export const PROJECTS: Project = [
     {
         name: "Hills of Glory Website",
-        image:"hog_site.png",
+        image:"/Projects/hog_site.png",
         description:"As the project leader, I guided our group in building a website for Hills of Glory Church in Mabalacat City, Pampanga. I quickly learned Laravel, Livewire, Flux UI, and Eloquent to contribute on both frontend and backend. I also provided documents and tutorials to help my teammates understand the stack and work more effectively. Throughout the project, I helped organize our tasks and made sure we built a website that truly fit the church’s needs.",
         skills:[
             'Laravel',
@@ -17,7 +17,7 @@ export const PROJECTS: Project = [
     },
     {
         name: "Mini HCM Time Tracking System",
-        image:"time_tracking.png",
+        image:"/Projects/time_tracking.png",
         link:"https://mini-hcm-time-tracking.vercel.app",
         description:"Project for Cornerstone Assessment. Developed a mini HCM Time Tracking System using Next.js, TypeScript, Firebase, and Tailwind CSS. Implemented user authentication, time tracking features, and a responsive UI to enhance user experience.",
         skills:[
@@ -30,7 +30,7 @@ export const PROJECTS: Project = [
     },
     {
         name: "Library",
-        image:"library_project.png",
+        image:"/Projects/library_project.png",
         description:"Successfully completed a technical assessment for Citrof Group, quickly learning Next.js, Prisma, and PostgreSQL in just three days, demonstrating adaptability, problem-solving, and a strong ability to learn new technologies under tight deadlines.",
         skills:[
             'Next.js',
@@ -45,7 +45,7 @@ export const PROJECTS: Project = [
     },
     {
         name: "LighString Vintage Guitar Shop",
-        image:"lightstring.png",
+        image:"/Projects/lightstring.png",
         link:"https://www.facebook.com/profile.php?id=61575701668511",
         description:"I create and edit posters, images, videos, and slides to enhance branding and engagement for Lightstring Vintage Guitars Shop.",
         skills:[
@@ -55,8 +55,19 @@ export const PROJECTS: Project = [
         ]
     },
     {
+        name: "Safe Travel PH - Data Presenter",
+        image:"/Projects/safetravelph.png",
+        link:"https://jorielbrian.shinyapps.io/ISTOPP_Rshiny/?fbclid=IwY2xjawPR6FVleHRuA2FlbQIxMABicmlkETE1Ymt2U1lEdWJFWnEzcldXc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHuEqSq6-TKloOzJm45_86QokqFSJweLi4i5vXZH3QEhNlyGLqYQjxAQsJzte_aem_nnf-O7sHLbL8Z7wSyuGplg",
+        description:"Developed a travel website for Safe Travel PH Data Presenter, focusing on compiling data for Safe Travel PH and generating presentations that is understandable by viewers.",
+        skills:[
+            'R',
+            'Shiny',
+            'SQL'
+        ]
+    },
+    {
         name: "Website Portfolio",
-        image:"portfolio_project.png",
+        image:"/Projects/portfolio_project.png",
         link:"https://jorielbrian.github.io/JorielBrianSudario_Portfolio/",
         description:"My first portfolio, created when I was learning the fundamentals of front-end development.",
         skills:[
