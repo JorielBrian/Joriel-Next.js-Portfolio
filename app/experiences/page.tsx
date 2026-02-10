a008f6712be1c5b1dbddfdd542fe22da8d21a9c6
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-02-10 08:17:58 +0800

    fixed the page hooks

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 import { QUALIFICATIONS, QUALIFICATIONS_PROJECTS } from "../data/qualifications";
-import Experiences from "@/app/components/Cards/ExperienceCard";
+import Experiences from "@/components/Cards/ExperienceCard";
+import { useEffect } from "react";
 
 const Qualifications = () =>{
     if (typeof window !== 'undefined') {
+    useEffect(() => {
         window.scrollTo(0, 0);
+    }, []);
     }
     
-    return (
+    return (    
         <div className="w-4/5 mx-auto my-5 p-5 rounded-sm content-center overflow-hidden">
             { QUALIFICATIONS && <Experiences title="Key Qualifications" experiences={QUALIFICATIONS} /> }
             { QUALIFICATIONS_PROJECTS && <Experiences title="Contractual Projects" experiences={QUALIFICATIONS_PROJECTS}/> }
