c78163ddb6fd97ab3d0e2dad80373d9f8547bbde
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-02-10 12:51:07 +0800

    fixed the image and texts responsiveness

--- components/Sections/Summary.tsx
+++ components/Sections/Summary.tsx
@@ -10,14 +10,14 @@ const Summary = () =>{
                 <div className="card relative p-4 align-middle sm:rounded-2xl">
                     <div className="2xl:col-span-2 content-center">
                         <motion.img 
-                        initial={{x:100, opacity:0}} 
-                        whileInView={{x:0, opacity:1}} 
-                        transition={{ease:"easeIn", duration:1.5}}
-                        src="/Profile Picture.png" 
-                        alt="My Image" 
-                        className="hidden lg:block float-left w-fit m-4 size-120 my-5 rounded-full"
+                            initial={{x:100, opacity:0}} 
+                            whileInView={{x:0, opacity:1}} 
+                            transition={{ease:"easeIn", duration:1.5}}
+                            src="/Profile Picture.png" 
+                            alt="My Image" 
+                            className="hidden lg:block lg:float-left w-fit m-4 size-70  xl:size-90 2xl:120 my-5 rounded-full"
                         />
-                        <div className="flex text-white font-sans justify-center flex-col">
+                        <div className="text-white font-sans justify-center flex-col">
                             { PROFESSIONAL_SUMMARY && PROFESSIONAL_SUMMARY.map(summary => (
                                 <motion.p 
                                     key={summary}  
