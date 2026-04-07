21cbd24c61967d7d5ae2d476bd3c54e4b389cac0
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-04-07 11:27:27 +0800

    fixed animation

--- components/Sections/Experience.tsx
+++ components/Sections/Experience.tsx
@@ -19,7 +19,6 @@ const Experience = () => {
         id="experience"
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.3 }}
         transition={{ duration: 0.65, ease: "easeOut" }}
         className="py-16"
     >
