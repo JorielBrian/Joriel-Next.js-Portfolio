d2315a44fb014eb4636232650fa6f6d784949859
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-04-20 14:50:50 +0800

    changed the sections to ExperienceCard cards

--- app/(admin)/jb-admin/experiences/page.tsx
+++ app/(admin)/jb-admin/experiences/page.tsx
@@ -1,9 +1,25 @@
+import { FaPlus } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
 import { QUALIFICATIONS } from "@/data/qualifications";
+import ExperienceCard from "@/components/Cards/ExperienceCard";
+import Link from "next/link";
 
 const Experiences = () => {
   return (
     <main>
-        <h1 className='text-2xl font-bold'>Experiences</h1>
+        <Button
+            className="button active place-self-end"
+            variant="link"
+        >
+            <Link 
+                href="/jb-admin/experiences/create"
+                className="inline-flex items-center gap-2"
+            >
+                <FaPlus />
+                Add Experience
+            </Link>
+        </Button>
+        {QUALIFICATIONS && <ExperienceCard experiences={QUALIFICATIONS} admin={true} />}
     </main>
   )
 }
