b7165f4ffb2990588842675091a4d38833c29036
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-04-20 13:24:21 +0800

    added experiences page

---
+++ app/(admin)/jb-admin/experiences/page.tsx
@@ -0,0 +1,11 @@
+import { QUALIFICATIONS } from "@/data/qualifications";
+
+const Experiences = () => {
+  return (
+    <main>
+        <h1 className='text-2xl font-bold'>Experiences</h1>
+    </main>
+  )
+}
+
+export default Experiences
