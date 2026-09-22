0605a4281494bcfb9aab94419e917a53e038a4b6
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-09-22 19:18:01 +0800

    3D integration

--- components/Sections/Hero.tsx
+++ components/Sections/Hero.tsx
@@ -32,40 +32,62 @@ function Hero() {
   }, []);
 
   return (
-    <section id="hero" className="h-screen mb-20 xl:mb-0">
-        <motion.div 
-            initial={{y:100, opacity:0}}
-            whileInView={{y:0, opacity:1}}
-            transition={{ease:"easeIn", duration:1.5}}
-            className="w-full text-center font-sans"
-        >
-            <h1 className="text-5xl font-bold sm:text-7xl xl:text-8xl 2xl:text-9xl mb-2 tracking-tight">
-                Joriel Brian Sudario
-            </h1>
-            <h3 
-                className="pb-5 border-amber-50 text-3xl sm:text-4xl lg:text-5xl 2xl:text-7xl bg-linear-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"
+    <section
+        id="hero"
+        className="min-h-screen flex items-center"
+    >
+        <div className="w-full grid grid-cols-1 lg:grid-cols-2 items-center gap-8 lg:gap-4">
+
+            {/* LEFT — Content */}
+            <div>
+
+                <motion.div 
+                    initial={{y:100, opacity:0}}
+                    whileInView={{y:0, opacity:1}}
+                    transition={{ease:"easeIn", duration:1.5}}
+                    className="w-full text-center font-sans"
+                >
+                    <h1 className="text-5xl font-bold sm:text-7xl xl:text-8xl 2xl:text-9xl mb-2 tracking-tight">
+                        Joriel Brian Sudario
+                    </h1>
+                    <h3 
+                        className="pb-5 border-amber-50 text-3xl sm:text-4xl lg:text-5xl 2xl:text-7xl bg-linear-to-r from-neutral-400 via-cyan-500 to-blue-800 bg-clip-text text-transparent"
+                    >
+                        <Typewriter 
+                            // words={['Web Developer', 'Full Stack Developer', 'System Engineer', 'Software Engineer', 'Computer Programmer', 'IT Specialist', 'Circuit Assembler', 'Information Technology']}
+                            words={['Software Engineer', 'Full Stack Developer', 'Web Developer', 'System Engineer']}
+                            loop = {true}
+                            cursor cursorStyle = {'|'} cursorBlinking = {true} cursorColor="white"
+                        />
+                    </h3>
+                    <FocusSkills />
+                </motion.div>
+                <motion.div
+                    initial={{y:-100, opacity:0}}
+                    whileInView={{y:0, opacity:1}}
+                    transition={{ease:"easeIn", duration:1.5}}
+                    className="py-4 mt-10"
+                >
+                    <div className="text-center space-y-2">
+                        { INTRODUCTION && INTRODUCTION.map(intro => (
+                            <p key={intro} className="font-sans">{ intro }</p>
+                        )) }
+                    </div>
+                </motion.div>
+            </div>
+
+
+            {/* RIGHT — 3D */}
+            <motion.div
+                initial={{ x: 100, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ ease: "easeIn", duration: 1.5 }}
+                className="w-full h-105 lg:h-125"
             >
-                <Typewriter 
-                    // words={['Web Developer', 'Full Stack Developer', 'System Engineer', 'Software Engineer', 'Computer Programmer', 'IT Specialist', 'Circuit Assembler', 'Information Technology']}
-                    words={['Software Engineer', 'Full Stack Developer', 'Web Developer', 'System Engineer']}
-                    loop = {true}
-                    cursor cursorStyle = {'|'} cursorBlinking = {true} cursorColor="white"
-                />
-            </h3>
-            <FocusSkills />
-        </motion.div>
-        <motion.div
-            initial={{y:-100, opacity:0}}
-            whileInView={{y:0, opacity:1}}
-            transition={{ease:"easeIn", duration:1.5}}
-            className="py-4 mt-10"
-        >
-                <div className="text-center space-y-2">
-                    { INTRODUCTION && INTRODUCTION.map(intro => (
-                        <p key={intro} className="font-sans">{ intro }</p>
-                    )) }
-                </div>
-        </motion.div>
+                <DevWorkstation3D />
+            </motion.div>
+
+        </div>
     </section>
   )
 }
